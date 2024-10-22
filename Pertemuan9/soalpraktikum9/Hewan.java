115acf8501ff23343f210c8fdaa3ff62a8a0a1ff
Ferdi Maulana Ikhsan <ferdimaulanaikhsan@students.untidar.ac.id>
2024-10-22 08:56:21 +0700

    upload file kode pertemuan 9

---
+++ Pertemuan9/soalpraktikum9/Hewan.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package soalpraktikum9;
+
+/**
+ *
+ * @author HP
+ */
+abstract class Hewan {
+    
+    abstract void suara();
+   
+}
